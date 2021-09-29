6de04e4bd47cf2322d8d28938586608a32420fe4
Valerii Ivanov <valfrom@gmail.com>
2021-09-29 16:54:48 +0300

    Add timeout

--- pikarpc/rpc_client.py
+++ pikarpc/rpc_client.py
@@ -13,10 +13,20 @@ class RpcClient(object):
 
     def call(self, data):
         url = 'http://{0}:{1}/rpc/{2}'.format(self.host, self.port, self.queue_name)
-        r = requests.post(url, data=chunked_bytes_from_object(data), headers={'id': str(uuid.uuid4())})
+        try:
+            r = requests.post(url, data=chunked_bytes_from_object(data), headers={'id': str(uuid.uuid4())}, timeout=self.timeout)
+        except requests.exceptions.RequestException as e:
+            return None
+        except:
+            return None
+
         return chunked_object_from_bytes(r.content)
 
     def send(self, data):
         url = 'http://{0}:{1}/rpc/{2}'.format(self.host, self.port, self.queue_name)
-        requests.post(url, data=chunked_bytes_from_object(data), headers={'id': str(uuid.uuid4()),'type': 'async'})
-
+        try:
+            requests.post(url, data=chunked_bytes_from_object(data), headers={'id': str(uuid.uuid4()),'type': 'async'}, timeout=self.timeout)
+        except requests.exceptions.RequestException as e:
+            return
+        except:
+            return
--- pikarpc/rpc_server.py
+++ pikarpc/rpc_server.py
@@ -3,21 +3,33 @@ from .chunked_utils import chunked_bytes_from_object, chunked_object_from_bytes
 
 
 class RpcServer:
-    def __init__(self, queue_name='rpc_queue', host='localhost', port=8001):
+    def __init__(self, queue_name='rpc_queue', host='localhost', port=8001, timeout=120):
         self.queue_name = queue_name
         self.host = host
         self.port = port
+        self.timeout = timeout
 
     def wait(self):
         url = 'http://{0}:{1}/rpc/{2}'.format(self.host, self.port, self.queue_name)
-        requests.post(url, headers={'type': 'get'})
+        try:
+            requests.post(url, headers={'type': 'get'}, timeout=self.timeout)
+        except requests.exceptions.RequestException as e:
+            pass
+        except:
+            pass
 
     def start(self):
         url = 'http://{0}:{1}/rpc/{2}'.format(self.host, self.port, self.queue_name)
         while True:
-            req = requests.post(url, headers={'type': 'get'})
-            result = chunked_bytes_from_object(self.process(chunked_object_from_bytes(req.content)))
-            requests.post('http://{0}:{1}/'.format(self.host, self.port), data=result, headers={'id': req.headers['id'], 'type': 'result'})
+            try:
+                req = requests.post(url, headers={'type': 'get'}, timeout=self.timeout)
+                result = chunked_bytes_from_object(self.process(chunked_object_from_bytes(req.content)))
+                requests.post('http://{0}:{1}/'.format(self.host, self.port), data=result,
+                              headers={'id': req.headers['id'], 'type': 'result'}, timeout=self.timeout)
+            except requests.exceptions.RequestException as e:
+                pass
+            except:
+                pass
 
     def process(self, data):
         return data
